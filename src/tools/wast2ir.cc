0bb02567cc54d954e2393f2de2a2359a8bce4432
Jacob Gravelle <jgravelle@google.com>
2017-07-19 11:24:38 -0700

    Read function types off wabt Module

--- src/tools/wast2ir.cc
+++ src/tools/wast2ir.cc
@@ -24,10 +24,9 @@
 
 #include "config.h"
 
-#include "binary-writer.h"
-#include "binary-writer-spec.h"
 #include "common.h"
 #include "error-handler.h"
+#include "expr-visitor.h"
 #include "ir.h"
 #include "option-parser.h"
 #include "resolve-names.h"
@@ -39,14 +38,94 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
 
 using namespace wabt;
 
+#define CHECK_RESULT(expr)  \
+  do {                      \
+    if (Failed(expr))       \
+      return Result::Error; \
+  } while (0)
+
+class IRVisitor : public ExprVisitor::DelegateNop {
+public:
+  llvm::Module* ir;
+  llvm::LLVMContext context;
+  llvm::IRBuilder<> builder;
+
+  Module* module;
+  ExprVisitor visitor;
+
+  IRVisitor() : builder(context), visitor(this) {
+    ir = new llvm::Module("test", context);
+  }
+  ~IRVisitor() {
+    delete ir;
+  }
+
+  Result VisitModule(Module* module_) {
+    module = module_;
+
+    // for (Index i = 0; i < module->globals.size(); ++i)
+    //   CHECK_RESULT(VisitGlobal(i, module->globals[i]));
+    // for (Index i = 0; i < module->func_types.size(); ++i)
+    //   CHECK_RESULT(VisitFuncType(i, module->func_types[i]));
+    for (Func* func : module->funcs) {
+      CHECK_RESULT(VisitFunc(func));
+    }
+    // for (Index i = 0; i < module->tables.size(); ++i)
+    //   CHECK_RESULT(VisitTable(i, module->tables[i]));
+    // for (Index i = 0; i < module->memories.size(); ++i)
+    //   CHECK_RESULT(VisitMemory(i, module->memories[i]));
+
+    return Result::Ok;
+  }
+
+private:
+  Result VisitFunc(Func* func) {
+    llvm::FunctionType* funcType = ToLLVMFuncType(func);
+    llvm::Constant* c = ir->getOrInsertFunction(func->name, funcType);
+    return Result::Ok;
+  }
+
+  llvm::FunctionType* ToLLVMFuncType(Func* func) {
+    llvm::Type* retType = ReturnType(func);
+    llvm::FunctionType* funcType = llvm::FunctionType::get(retType, {}, false);
+    return funcType;
+  }
+
+  llvm::Type* ReturnType(Func* func) {
+    assert(func->GetNumResults() <= 1);
+    if (func->GetNumResults() == 0) {
+      return llvm::Type::getVoidTy(context);
+    } else {
+      return ToLLVMType(func->GetResultType(0));
+    }
+  }
+
+  llvm::Type* ToLLVMType(Type type) {
+    switch (type) {
+    case Type::I32:
+      return llvm::Type::getInt32Ty(context);
+    case Type::I64:
+      return llvm::Type::getInt64Ty(context);
+    case Type::F32:
+      return llvm::Type::getFloatTy(context);
+    case Type::F64:
+      return llvm::Type::getDoubleTy(context);
+    default:
+      WABT_FATAL("Unsupported wasm->llvm type: %d\n", type);
+    };
+  }
+};
+
+// ------ //
+
 static const char* s_infile;
 static const char* s_outfile;
 static int s_verbose;
-static WriteBinaryOptions s_write_binary_options;
 static bool s_validate = true;
 static WastParseOptions s_parse_options;
 
@@ -63,16 +142,12 @@ static void parse_options(int argc, char* argv[]) {
   parser.AddOption('v', "verbose", "Use multiple times for more info", []() {
     s_verbose++;
     s_log_stream = FileStream::CreateStdout();
-    s_write_binary_options.log_stream = s_log_stream.get();
   });
   parser.AddHelpOption();
   parser.AddOption("debug-parser", "Turn on debugging the parser of wast files",
                    []() { s_parse_options.debug_parsing = true; });
   parser.AddOption('o', "output", "FILE", "output wasm binary file",
                    [](const char* argument) { s_outfile = argument; });
-  parser.AddOption("debug-names",
-                   "Write debug names to the generated binary file",
-                   []() { s_write_binary_options.write_debug_names = true; });
   parser.AddOption("no-check", "Don't check for invalid modules",
                    []() { s_validate = false; });
   parser.AddArgument("filename", OptionParser::ArgumentCount::One,
@@ -81,15 +156,13 @@ static void parse_options(int argc, char* argv[]) {
   parser.Parse(argc, argv);
 }
 
-Result write_ir_file(const Module* module, const char* filename) {
-  llvm::LLVMContext context;
-  llvm::Module* ir = new llvm::Module("test", context);
-  printf("Woop: %s , %p\n", filename, ir);
-  ir->dump();
-  // std::ofstream file;
-  // file.open(filename, std::ios::out | std::ios::binary);
-  // ir->print(file, nullptr);
-  return Result::Ok;
+Result write_ir_file(Module* module, const char* filename) {
+  IRVisitor visitor;
+  Result result = visitor.VisitModule(module);
+  puts("Parsed module:");
+  visitor.ir->dump();
+  // TODO: actually write to file
+  return result;
 }
 
 int ProgramMain(int argc, char** argv) {
@@ -113,7 +186,7 @@ int ProgramMain(int argc, char** argv) {
     result = validate_script(lexer.get(), script, &error_handler);
 
   if (Succeeded(result)) {
-    const Module* module = script->GetFirstModule();
+    Module* module = script->GetFirstModule();
     if (module) {
       result = write_ir_file(module, s_outfile);
     } else {
